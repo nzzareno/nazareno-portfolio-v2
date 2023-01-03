338aaee78fcd94926a1351a4d715f945b0941092
GarmaNazareno <garmatorresnazareno@gmail.com>
2023-01-03 01:18:10 -0300

    fff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import { Head, Html, Main, NextScript } from 'next/document';
-import { i18n } from '../next-i18next.config';
 
 export default function Document(props) {
-  const currentLocale = i18n.defaultLocale;
   return (
-    <Html lang={currentLocale}>
+    <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon-32x32.png" />
         <link
