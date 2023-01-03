9116a56c902d8c170666d26bf1afde5bbf2b473f
GarmaNazareno <garmatorresnazareno@gmail.com>
2023-01-03 00:22:40 -0300

    final-design-v1?

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es'],
-    localePath: path.resolve('./public/locales')
+    locales: ['en', 'es']
   }
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import { Head, Html, Main, NextScript } from 'next/document';
+import { i18n } from '../next-i18next.config';
 
 export default function Document(props) {
+  const currentLocale = i18n.defaultLocale;
   return (
-    <Html lang="en">
+    <Html lang={currentLocale}>
       <Head>
-      <link rel="shortcut icon" href="/favicon-32x32.png" />
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@700;800&display=swap"
           rel="stylesheet"
@@ -18,7 +20,6 @@ export default function Document(props) {
           href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
           rel="stylesheet"
         />
- 
       </Head>
       <body className="prose-headings:font-headings">
         <Main />
